4c90925fbf633b7d689a97264169d905bf59ffa1
Juan Felipe <juan_fe_7@live.com>
2021-09-13 22:04:43 -0500

    Included functions counting and modifying the state

--- my-wave-portal/contracts/WavePortal.sol
+++ my-wave-portal/contracts/WavePortal.sol
@@ -5,8 +5,27 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    // state variables
+    uint totalWaves; // undefined integer have initial values of 0 in solidity
+    uint256 initialGas = gasleft();
+
     constructor() {
-        console.log("Hey! Time to fly!");
+        console.log("Hey!", "|", " Available gas:", initialGas);
+    }
+
+    function wave() public { // has access to declared variable
+        totalWaves += 1;
+        console.log("%s is waved!", msg.sender );
+    }
+
+    // View function declares that no state will be changed.
+    function getTotalWaves() public view returns (uint) {
+        console.log("We have %d total waves", totalWaves);
+        return totalWaves;
+    }
+
+    function getGasLeft() public view returns (uint256) {
+        return initialGas - gasleft();
     }
 }
 
