a76da439d0e9acf49bf6ff720790f21385788baa
Juan Felipe <juan_fe_7@live.com>
2021-09-21 21:17:38 -0500

    Finished Metamask wallet connection

--- wave_visuals/src/App.js
+++ wave_visuals/src/App.js
@@ -1,8 +1,50 @@
 import * as React from 'react';
 //import { ethers } from 'ethers';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [currentAccount, setCurrentAccount] = useState('');
+
+  const walletIsConnected = async () => {
+    const { ethereum: eth } = window; // Injected by Metamask into the browser
+    // TODO: Change this to a notification handler function. No alerts.
+    if (!eth) {
+      alert('Hey! Remember to install your Metamask wallet!');
+    } else {
+      console.log(eth);
+    }
+
+    const authorizedAccount = await eth.request({ method: 'eth_accounts' });
+    if (authorizedAccount.length > 0) {
+      const account = authorizedAccount[0];
+      setCurrentAccount(account); // Saves current account;
+      console.log(`Current account: ${account}`);
+    } else {
+      console.log('No authorized Metamask account');
+    }
+  };
+
+  const connectWallet = async () => {
+    const { ethereum: eth } = window;
+    if (!eth) return alert('Get Metamask!'); // TODO: Notification handler
+    try {
+      const accountRequest = await eth.request({
+        method: 'eth_requestAccounts',
+      });
+      const accountToUse = accountRequest[0];
+      console.log(`Connected! ${accountToUse}`);
+
+      setCurrentAccount(accountToUse);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    walletIsConnected();
+  }, []);
+
   const wave = () => {};
   return (
     <div className='mainContainer'>
@@ -15,8 +57,13 @@ function App() {
         </div>
 
         <button className='waveButton' onClick={wave}>
-          Wave at Me
+          Wave at Me!
         </button>
+        {currentAccount ? null : (
+          <button className='waveButton' onClick={connectWallet}>
+            Connect your Wallet
+          </button>
+        )}
         <p></p>
       </div>
       <footer>
