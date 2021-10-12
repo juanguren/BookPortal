96b3eb1b2b3fdbdc8bd696118f878279fa3d5f72
Juan Felipe <juan_fe_7@live.com>
2021-10-11 22:14:10 -0500

    Abstracted away the balance checking method

--- my-wave-portal/scripts/modules/book_methods.js
+++ my-wave-portal/scripts/modules/book_methods.js
@@ -1,17 +1,34 @@
 const testComplementMethods = async (contract, owner) => {
-  // * Method 1
-  let totalBooks = await contract.getTotalBookData();
-  totalBooks.map((book) => {
-    console.log({ title: book.book_name });
-  });
+  try {
+    // * Method 1
+    let totalBooks = await contract.getTotalBookData();
+    totalBooks.map((book) => {
+      console.log({ title: book.book_name });
+    });
 
-  // * Method 2
-  const totalOfBooks = await contract.getTotalBookCount();
-  console.log({ totalOfBooks: totalOfBooks.toString() });
+    // * Method 2
+    const totalOfBooks = await contract.getTotalBookCount();
+    console.log({ totalOfBooks: totalOfBooks.toString() });
 
-  // * Method 3
-  const userBookCount = await contract.getBookCountPerUser(owner.address);
-  console.log({ userBookCount: userBookCount.toString() });
+    // * Method 3
+    const userBookCount = await contract.getBookCountPerUser(owner.address);
+    console.log({ userBookCount: userBookCount.toString() });
+  } catch (error) {
+    return { error };
+  }
 };
 
-module.exports = { testComplementMethods };
+const checkForBalance = async (contract) => {
+  try {
+    const foundBalance = await Headers.ethers.provider.getBalance(
+      contract.address
+    );
+    return {
+      balance: hre.ethers.utils.formatEther(foundBalance),
+    };
+  } catch (error) {
+    return error;
+  }
+};
+
+module.exports = { testComplementMethods, checkForBalance };
